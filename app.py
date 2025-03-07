cad33f6de6635075b7a9178c11a139cc414c58c6
23495A0516 <23495A0516@qiscet.edi.in>
2025-03-07 22:26:12 +0530

    Updated requirements to fix Render deployment

--- app.py
+++ app.py
@@ -250,4 +250,5 @@ def static_files(path):
     return send_from_directory(os.path.join(app.root_path, 'static'), path)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get PORT from environment, default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
